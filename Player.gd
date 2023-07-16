6f166c24ccf04600382a3923af826dd3f6ee4144
Dan Brook <dan@broquaint.com>
2023-07-16 19:32:05 +0700

    Functioning movement code
    Too lazy to implement it myself I have cribbed from someone who knows
    what they are doing. Thank you, wiebow!

--- Player.gd
+++ Player.gd
@@ -1,26 +1,40 @@
-extends KinematicBody2D
+extends Area2D
 
-# https://kidscancode.org/godot_recipes/3.x/2d/topdown_movement/index.html
+const THRUST : float = 10.0
+const MAX_SPEED : float = 400.0
+const ROTATION_SPEED : float = 5.0 * 60
 
-var speed = 200
-var rotation_speed = 1.5
+var dead : bool
+var velocity : Vector2
 
-var velocity = Vector2.ZERO
-var rotation_dir = 0
+func _physics_process(delta) -> void:
+	if dead == true:
+		return
 
-func get_input():
-	rotation_dir = 0
-	velocity = Vector2.ZERO
-	if Input.is_action_pressed('move_right'):
-		rotation_dir += 1
-	if Input.is_action_pressed('move_left'):
-		rotation_dir -= 1
-	if Input.is_action_pressed('move_down'):
-		velocity -= transform.x * speed
-	if Input.is_action_pressed('move_up'):
-		velocity += transform.x * speed
+	if Input.is_action_pressed("move_left"):
+		rotation_degrees -= delta * ROTATION_SPEED
+	elif Input.is_action_pressed("move_right"):
+		rotation_degrees += delta * ROTATION_SPEED
 
-func _physics_process(delta):
-	get_input()
-	rotation += rotation_dir * rotation_speed * delta
-	velocity = move_and_slide(velocity)
+	# get acceleration if thrust is pressed
+	if Input.is_action_pressed("move_up"):
+		var acceleration : Vector2
+
+		# -THRUST because vector pointing up = y value of -1, and
+		# rotated() method of Vector2 needs a radian, not degrees,
+		# so convert that using deg2rad
+		acceleration = Vector2(0, -THRUST).rotated(deg2rad(rotation_degrees))
+
+		# add acceleration to current speed
+		velocity += acceleration
+
+	# dampen a bit
+	velocity *= 0.98
+
+	# cap speed
+	if velocity.length() > MAX_SPEED:
+		velocity = velocity.normalized() * MAX_SPEED
+		# velocity vector is added to position in BaseObject.gd
+
+	position += velocity * delta
+
--- Root.gd
+++ Root.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$KinematicBody2D.position = Vector2(96, 96)
+	$Player.position = Vector2(96, 96)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
