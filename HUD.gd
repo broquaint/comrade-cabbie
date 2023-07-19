0b80f9815a38d3bb14529472a0fb91cfcd543cd2
Dan Brook <dan@broquaint.com>
2023-07-19 20:02:41 +0700

    Drop old "embedded points" from Root
    Much cleaner.

--- HUD.gd
+++ HUD.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func on_new_pickup(dropoff: Node):
-	$Container/Destination.text = dropoff.name
+	$Container/Destination.text = dropoff.point_name
 
 func on_new_dropoff():
 	$Container/Destination.text = '...'
--- PickupPoint.gd
+++ PickupPoint.gd
@@ -4,4 +4,4 @@ export var point_name : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$Label.text = point_name
